124a11b2784218ebf933aa37a3c23562fb020c5b
Shahfarzane <42602105+Shahfarzane@users.noreply.github.com>
2021-05-18 17:02:44 +0200

    Showing popup window on map

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,6 +1,13 @@
 import { User } from "./User";
 import { Company } from "./Company";
 
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -14,22 +21,20 @@ export class CustomMap {
     });
   }
 
-  addMarker(mappable: User | Company): void {
-    new google.maps.Marker({
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng,
       },
     });
+
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: "Hi there",
+      });
+      infoWindow.open(this.googleMap, marker);
+    });
   }
-  //   addCompanyMarker(company: Company): void {
-  //     new google.maps.Marker({
-  //       map: this.googleMap,
-  //       position: {
-  //         lat: company.location.lat,
-  //         lng: company.location.lng,
-  //       },
-  //     });
-  //   }
 }
